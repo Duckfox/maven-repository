0fbc8f65306db63f94126f0d44604f74d58dd369
Duck_fox <2660759310@qq.com>
2024-06-24 22:40:43 +0800

    修改Pixelmon8.4.3

--- Pull-Commit-Push.bat
+++ Pull-Commit-Push.bat
@@ -1,16 +1,16 @@
 @echo off
-echo 正在拉取最新代码
+echo 正在拉取最新代码...
 setlocal enabledelayedexpansion
 
 :: 拉取最新代码
 git pull origin master
 
 :: 提示用户输入提交消息
-set /p commitMessage=Enter the commit message:
+set /p commitMessage=请输入提交信息:
 
 :: 检查是否有输入
 if "!commitMessage!"=="" (
-    echo Commit message is required.
+    echo 未输入提交信息，程序退出.
     exit /b 1
 )
 
@@ -23,5 +23,5 @@ git commit -m "!commitMessage!"
 :: 推送到远程仓库
 git push origin master
 
-echo Done.
+echo 上传完毕.
 pause
